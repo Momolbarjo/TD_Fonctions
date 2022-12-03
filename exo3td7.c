de0ac74fddd97c526e38fd961bf76eef350e3f31
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-03 14:05:07 +0100

    Add files via upload

---
+++ exo3td7.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void CompareNumber (int a,int b) {
+
+
+
+b=rand()%101;
+srand(time(NULL));
+
+
+for (int i=1;i<=11;i++) {
+    scanf("%d",&a);
+   if(a<b) {
+
+        printf("Plus grand \n");
+
+        if (i==11){
+
+            printf("sale merde ta perdu");
+        }
+
+
+   }
+
+    else if(a>b) {
+
+        printf("Plus petit\n");
+
+
+        if (i==11){
+
+            printf("sale merde ta perdu");
+        }
+
+
+
+   }
+
+    else if (a==b) {
+
+        printf("Trouve !");
+
+        break;
+
+   }
+
+printf("\n");
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void main () {
+
+
+srand(time(NULL));
+
+int a,b;
+
+
+ printf("Quel est la valeur secrete :");
+
+
+    CompareNumber(a, b);
+
+
+
+}
