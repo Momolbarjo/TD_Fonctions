03c0f1e1b643b77ebb7c206a3dc4d609b78646e5
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-06 09:12:14 +0100

    Add files via upload

---
+++ exo7.c
@@ -0,0 +1,34 @@
+/* elle retoune si le format est correct le nombre total de caractère saisi ,si le format n'est pas correct, elle retourne une valeur négative */
+
+
+#include <stdio.h>
+
+void main () 
+{
+	int a=0;
+	a=printf("???");
+	printf("\n a=%d \n",a);
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+ 
