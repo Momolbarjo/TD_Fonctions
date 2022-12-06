b1a01c37ab1dab1155328f17e866e9b8c344219c
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-06 09:57:21 +0100

    Add files via upload

---
+++ exo8.c
@@ -0,0 +1,42 @@
+/* Lors du succès, la fonction retourne le nombre d'objets provenant de la liste d'arguments qui ont pu bien etre lu*/
+
+
+#include <stdio.h>
+
+int askUserInt (int min, int max)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	printf("Entrez un caractere :");
+	a=scanf("%d",&b);
+	a=0;
+	 printf("\na=%d\n",a);
+	printf("Entrez un entier :");
+	a=scanf("%d",&c);
+	a=1;
+	 printf("\na=%d\n",a);
+return 0;
+}
